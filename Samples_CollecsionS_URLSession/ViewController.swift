b03ebe8cedd1577fa1de27c35ef7d20b852a6d8a
Dmitry Parkhomenko <oku_74@mail.ru>
2024-01-31 08:55:29 +0500

    Initial Commit

---
+++ Samples_CollecsionS_URLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Samples_CollecsionS_URLSession
+//
+//  Created by Dmitry Parhomenko on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
