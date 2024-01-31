ccb42cd363e4c9c766f95a12c36d45fc6e6edafa
Dmitry Parkhomenko <oku_74@mail.ru>
2024-01-31 10:44:15 +0500

    Created a DataSstore with data

---
+++ Samples_CollecsionS_URLSession/Model/Course.swift
@@ -0,0 +1,8 @@
+//
+//  Course.swift
+//  Samples_CollecsionS_URLSession
+//
+//  Created by Dmitry Parhomenko on 31.01.2024.
+//
+
+import Foundation
---
+++ Samples_CollecsionS_URLSession/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  Samples_CollecsionS_URLSession
+//
+//  Created by Dmitry Parhomenko on 31.01.2024.
+//
+
+import Foundation
